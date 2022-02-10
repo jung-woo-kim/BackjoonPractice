3c7010278de621b2aecc17861226b915274d3f63
jungwoo <rlawjddn103@naver.com>
2022-02-10 16:37:02 +0900

    Add PriorityQueueAbs

---
+++ PriorityQueueAbs.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+
+public class PriorityQueueAbs {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        java.util.PriorityQueue<Integer> pr = new java.util.PriorityQueue<>(100000, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+
+                int abs1 = Math.abs(o1);
+                int abs2 = Math.abs(o2);
+
+                if(abs1 == abs2) return o1 > o2 ? 1 : -1;
+
+                return abs1 - abs2;
+
+            }
+        });
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] list = new int[n];
+
+        for (int i = 0; i < n; i++){
+            list[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < n; i++){
+            if (list[i] == 0){
+                if (pr.isEmpty()){
+                    System.out.println(0);
+                }else{
+                    System.out.println(pr.poll());
+                }
+            }else{
+                pr.add(list[i]);
+            }
+        }
+    }
+}
--- out/production/Algorithms/README.md
+++ out/production/Algorithms/README.md
@@ -3,7 +3,7 @@
 
 1. StringBuilder 및 BufferReader 사용해야 시간 더 조금 걸림
 
-2. 제출시 Main Class, 커밋 시 문제명
+2. 제출시 PriorityQueue2 Class, 커밋 시 문제명
  
 
 각 구현은 .java 파일로 저장
