b8198bbe32a95a821dd12368ca743fa424f75593
jungwoo <rlawjddn103@naver.com>
2022-02-11 01:25:25 +0900

    Add DFS4

---
+++ DFS4.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class DFS3 {
+
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+
+        dfs(0, N,M,0);
+        System.out.print(sb);
+
+    }
+
+    public static void dfs(int start,int N,int M ,int depth) {
+
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+
+            arr[depth] = i + 1;
+            dfs(0, N, M, depth + 1);
+
+        }
+    }
+
+}
