56e44a0b4a8d91974b5c86adb6495b64c4920377
jungwoo <rlawjddn103@naver.com>
2022-02-17 20:17:17 +0900

    Add Editor1406

---
+++ stack/Editor1406.java
@@ -0,0 +1,56 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(reader.readLine());
+        Stack<String> left = new Stack<>();
+        Stack<String> right = new Stack<>();
+
+        for(int i=0; i<s.length(); i++)
+            left.push(s.substring(i,i+1));
+
+
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            String c = st.nextToken();
+            switch(c) {
+                case "L":
+                    if(!left.isEmpty())
+                        right.push(left.pop());
+                    break;
+                case "D":
+                    if(!right.isEmpty())
+                        left.push(right.pop());
+                    break;
+                case "B":
+                    if(!left.isEmpty())
+                        left.pop();
+                    break;
+                case "P":
+                    left.push(st.nextToken());
+                    break;
+            }
+        }
+
+        while(!left.isEmpty()) //오른쪽 스택에 왼쪽 스택 옮기기
+            right.push(left.pop());
+
+        while(!right.isEmpty())
+            sb.append(right.pop());
+
+        System.out.print(sb);
+    }
+
+}
