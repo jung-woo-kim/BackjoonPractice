c9b6ece5b686702f829df80f27bf988f64152d5b
jungwoo <rlawjddn103@naver.com>
2022-02-20 13:35:48 +0900

    Add Padovan9461

---
+++ dp/Padovan9461.java
@@ -0,0 +1,46 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static long[] P;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+
+        P = new long[101];
+
+        int T = Integer.parseInt(br.readLine());
+
+
+        P[1] = 1;
+        P[2] = 1;
+        P[3] = 1;
+        P[4] = 2;
+        P[5] = 2;
+
+        for (int i = 0; i < T; i++){
+            int N = Integer.parseInt(br.readLine());
+
+            sb.append(p(N)).append('\n');
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static long p(int N){
+        if (P[N] == 0){
+            P[N] = p(N-2) + p(N-3);
+        }
+        return P[N];
+    }
+}
