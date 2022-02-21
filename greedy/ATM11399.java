73e63f570a8400b845f3bb6ad20b9c86394162da
jungwoo <rlawjddn103@naver.com>
2022-02-22 02:17:27 +0900

    Add ATM11399

--- greedy/ATM11399.java
+++ greedy/ATM11399.java
@@ -9,7 +9,6 @@ import java.util.StringTokenizer;
 public class ATM11399 {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
