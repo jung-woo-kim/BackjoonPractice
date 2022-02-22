063585b8f00d0cd110c11c83b5f373de15d65957
jungwoo <rlawjddn103@naver.com>
2022-02-22 15:18:38 +0900

    Add BOJ1541

--- greedy/BOJ1541.java
+++ greedy/BOJ1541.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ1541 {
 
     public static void main(String[] args) throws IOException {
 
