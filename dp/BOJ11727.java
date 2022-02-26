2be3270258531570a5ecb00082fea2494c946e47
jungwoo <rlawjddn103@naver.com>
2022-02-27 02:31:55 +0900

    Add BOJ1177

---
+++ dp/BOJ11727.java
@@ -0,0 +1,37 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11727 {
+
+    static Integer[] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new Integer[1001];
+
+        dp[1] = 1;
+        dp[2] = 3;
+
+        System.out.println(dp(N));
+
+    }
+
+    static int dp(int n){
+        if (dp[n] == null){
+            dp[n] = (dp(n-1) + dp(n-2) * 2) % 10007;
+        }
+        return dp[n];
+    }
+
+}
