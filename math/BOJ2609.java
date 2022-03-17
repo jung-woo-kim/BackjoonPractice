8d621ff7b72003242d3a760f231af00f99c3eb5d
jungwoo <rlawjddn103@naver.com>
2022-03-18 00:27:05 +0900

    BOJ2609

--- math/BOJ2609.java
+++ math/BOJ2609.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ2609 {
     static int N,M;
 
     public static void main(String[] args) throws IOException {
